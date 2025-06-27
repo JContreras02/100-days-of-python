4c474880a2e8dcff10f491498a90b5887f8f8e43
JContreras14 <Jonathan.Contreras.JC1@gmail.com>
2025-06-27 00:32:59 -0400

    extra day 8 prac

--- Days/Day-8/day8_prac.py
+++ Days/Day-8/day8_prac.py
@@ -1,7 +1,15 @@
-
+'''
 def life_in_weeks(age):
     years_left = 90 - age
     weeks_left = years_left * 52
     print(f"You have {weeks_left} weeks left.")
     
 life_in_weeks(20)
+'''
+
+def greet_with(name, location):
+    print(f"Hello {name} from {location}, its a pleasure to meet you!")
+
+name = input("Hello! What's your name? ")
+location = input("Where are you located? ")
+greet_with(name, location)
