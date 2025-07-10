440c8b13c7982d98e7b8a731f1fa702d935bc9d3
JContreras14 <Jonathan.Contreras.JC1@gmail.com>
2025-07-10 16:02:26 -0400

    secret auction done

--- Days/Day-9/day9_proj.py
+++ Days/Day-9/day9_proj.py
@@ -1,44 +1,46 @@
 import art
 print(art.logo)
+print("Welcome to the secret auction! \n")
 
-# TODO-1: Ask the user for input                        DONE   
-# TODO-2: Save data into dictionary {name: price}       DONE
-# TODO-3: Whether if new bids need to be added          DONE
-# TODO-4: Compare bids in dictionary
-
+# Function to help find the highest bidder
+def find_highest_bidder(bidding_dictionary):
+    highest_bidder = 0
+    winner = ""
+    for bidder in bidding_dictionary:
+        bid_amount = bidding_dictionary[bidder]
+        if bid_amount > highest_bidder:
+            highest_bidder = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with a bid of ${highest_bidder}!")
 
 # Setting loop to true then later setting it to false when done
 program = True
 
 # Empty dictionary to store keys and values
-bidding_dict = {}
-highest_bidder = [0]
+bids = {}
 
-
-# while loop that will continue asking for names and bids until user says no
+# While loop that will continue asking for names and bids until user says no
 while program is True:
 
     name = input("What is your name?: ")
+    try:
+        price = int(input("What is your bid?: $"))
+    except ValueError:
+        print("Did not recognize input. Bidding has ended.")
+        break
 
-    bid = int(input("What is your bid?: $"))
-    bidding_dict[name] = bid
+    bids[name] = price
 
     #if statement to decide if there are more bidders or not 
-    
-    other_bidders = input("Are there any other bidders? Type 'yes' or 'no'.  ").lower()
+    other_bidders = input("Are there any other bidders? Type 'yes' or 'no':  \n").lower()
     if other_bidders == "yes":
-        continue
+        print("\n" * 50)
+        print(art.logo)
+
     elif other_bidders == "no":
-        # call function
-
         program = False
+        find_highest_bidder(bids)
     else:
-        print("Did not recognize input. Bidding has ended.")
-
-print(bidding_dict)
-
-# finish function
-def find_highest_bidder():
-    for key in bidding_dict[key]:
-            if bidding_dict[key] >= highest_bidder[0]:
-                highest_bidder[0] = bidding_dict[key]
+        print("Invalid input. Ending bidding.")
+        program = False
+        find_highest_bidder(bids)
