fee694568a3c749de83fe21dc0faa0396149b6b2
JContreras14 <Jonathan.Contreras.JC1@gmail.com>
2025-07-09 23:52:30 -0400

    secret auction work pt 2

--- Days/Day-9/day9_proj.py
+++ Days/Day-9/day9_proj.py
@@ -3,7 +3,7 @@ print(art.logo)
 
 # TODO-1: Ask the user for input                        DONE   
 # TODO-2: Save data into dictionary {name: price}       DONE
-# TODO-3: Whether if new bids need to be added
+# TODO-3: Whether if new bids need to be added          DONE
 # TODO-4: Compare bids in dictionary
 
 
@@ -12,15 +12,33 @@ program = True
 
 # Empty dictionary to store keys and values
 bidding_dict = {}
+highest_bidder = [0]
 
 
 # while loop that will continue asking for names and bids until user says no
 while program is True:
+
     name = input("What is your name?: ")
 
     bid = int(input("What is your bid?: $"))
     bidding_dict[name] = bid
 
-    program = False
+    #if statement to decide if there are more bidders or not 
+    
+    other_bidders = input("Are there any other bidders? Type 'yes' or 'no'.  ").lower()
+    if other_bidders == "yes":
+        continue
+    elif other_bidders == "no":
+        # call function
+
+        program = False
+    else:
+        print("Did not recognize input. Bidding has ended.")
 
 print(bidding_dict)
+
+# finish function
+def find_highest_bidder():
+    for key in bidding_dict[key]:
+            if bidding_dict[key] >= highest_bidder[0]:
+                highest_bidder[0] = bidding_dict[key]
