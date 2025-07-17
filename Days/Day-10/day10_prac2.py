f7a805e0bb0cdfbad1e993eb33cb87030d3deae5
JContreras14 <Jonathan.Contreras.JC1@gmail.com>
2025-07-16 21:38:20 -0400

    added 2 solutions

--- Days/Day-10/day10_prac2.py
+++ Days/Day-10/day10_prac2.py
@@ -1,4 +1,18 @@
 def is_leap_year(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+
+# One liner solution
+def is_leap_year(year):
     return (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)
 
 year_input = int(input("Please enter a year: "))
