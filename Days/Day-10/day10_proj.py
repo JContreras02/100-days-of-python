46e1f6712a8dca96e6c6152b51e454bbda90b5ce
JContreras14 <Jonathan.Contreras.JC1@gmail.com>
2025-07-21 12:45:54 -0400

    final calculator

--- Days/Day-10/day10_proj.py
+++ Days/Day-10/day10_proj.py
@@ -1,6 +1,7 @@
 import art
 print(art.logo)
 
+# Operation functions
 def add(n1, n2):
     return n1 + n2
 
@@ -11,6 +12,8 @@ def multiply(n1, n2):
     return n1 * n2
 
 def divide(n1, n2):
+    if n2 == 0:
+        return "Error: Division by zero"
     return n1 / n2
 
 operators_dict = {
@@ -20,20 +23,33 @@ operators_dict = {
     "/": divide
 }
 
-number_1 = float(input("What's your first number?: "))
-operator_list = "+-*/"
-for char in operator_list:
-    print(char)
-operator = input("Pick an operator: ")
-number_2 = float(input("What's the next number?: "))
+# Function for calculation will keep going until loop is false
+def calculator():
+    should_continue = True
+    num1 = float(input("What's your first number?: "))
 
-print(f"{number_1} {operator} {number_2} = {operators_dict[operator](number_1, number_2)}")
+    while should_continue:
+        for symbol in operators_dict:
+            print(symbol)
+        operator = input("Pick an operator: ")
 
-# Program asks the user to type the first number.
-# Program asks the user to type a mathematical operator (a choice of "+", "-", "*" or "/")
-# Program asks the user to type the second number.
-# Program works out the result based on the chosen mathematical operator.
-# Program asks if the user wants to continue working with the previous result.
-# If yes, program loops to use the previous result as the first number and then repeats the calculation process.
-# If no, program asks the user for the fist number again and wipes all memory of previous calculations.
-# Add the logo from art.py
+        if operator not in operators_dict:
+            print("Invalid operator. Try again.")
+            continue
+
+        num2 = float(input("What's the next number?: "))
+        result = operators_dict[operator](num1, num2) 
+        print(f"{num1} {operator} {num2} = {result}")
+        next_step = input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation. ").lower()
+
+        if next_step == 'y':
+            num1 = result
+            print("\n" * 3)
+        elif next_step == 'n':
+            print("\n" * 20)
+            should_continue = False
+        else:
+            print("Goodbye!")
+            return
+
+calculator()
