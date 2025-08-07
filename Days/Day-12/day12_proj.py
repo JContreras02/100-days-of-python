25c45ff58b152c222bb81a8466eee1951f001b40
JContreras14 <Jonathan.Contreras.JC1@gmail.com>
2025-08-07 15:28:10 -0400

    game_logic func

--- Days/Day-12/day12_proj.py
+++ Days/Day-12/day12_proj.py
@@ -3,23 +3,48 @@ import random
 
 print(logo)
 
-def game_logic(diff_level):
-    return
-
-
+def game_logic(rand_num, attempts):
+    guess = int(input("Make a guess: "))
+    if guess == rand_num:
+        print(f"You got it! The answer was {rand_num}.")
+    elif guess > rand_num:
+        while attempts != 0:
+            attempts -= 1
+            print(f"Too high. Guess Again.")
+            print(f"You have {attempts} attempts remaining to guess the number.\n")
+            game_logic(rand_num, attempts)
+        print(f"You've run out of guesses. The number was {rand_num}.")
+        print("Please re-run the code to play again!")
+    elif guess < rand_num:
+        while attempts != 0:
+            attempts -= 1
+            print(f"Too low. Guess Again.")
+            print(f"You have {attempts} attempts remaining to guess the number.\n")
+            game_logic(rand_num, attempts)
+        print(f"You've run out of guesses. The number was {rand_num}.")
+        print("Please re-run the code to play again!")
+    else:
+        while attempts != 0:
+            attempts -= 1
+            print(f"Guess Again.")
+            print(f"You have {attempts} attempts remaining to guess the number.\n")
+            game_logic(rand_num, attempts)
+        print(f"You've run out of guesses. The number was {rand_num}.")
+        print("Please re-run the code to play again!")
 
 print("Welcome to the Number Guessing Game!")
 print("Im thinking of a number between 1 and 100.")
-difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower
-random_number = random.randint(1,100)
+difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
+rand_num = random.randint(1,100)
 
 if difficulty == "easy":
     attempts = 10
-
+    print(f"You have {attempts} remaining to guess the number.\n")
+    game_logic(rand_num, attempts)
 elif difficulty == "hard":
     attempts = 5
-else:
-    print("Please enter a difficulty. Try Again")
+    print(f"You have {attempts} remaining to guess the number.\n")
+    game_logic(rand_num, attempts)
 
 
 
