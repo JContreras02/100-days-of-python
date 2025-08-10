65908d973f7674c24c57904d756eeaa7bd6b3f21
JContreras14 <Jonathan.Contreras.JC1@gmail.com>
2025-08-10 00:49:55 -0400

    final commit

--- Days/Day-12/day12_proj.py
+++ Days/Day-12/day12_proj.py
@@ -6,38 +6,19 @@ print(logo)
 EASY_LEVEL_TURNS = 10
 HARD_LEVEL_TURNS = 5
 
-'''
-def game_logic(rand_num, attempts):
-    guess = int(input("Make a guess: "))
+def check_answer(guess, rand_num, turns):
+    """Checks answer with guess and returns num of turns remaining."""
+    print("\n")
     if guess == rand_num:
         print(f"You got it! The answer was {rand_num}.")
     elif guess > rand_num:
-        while attempts != 0:
-            attempts -= 1
-            print(f"Too high. Guess Again.")
-            print(f"You have {attempts} attempts remaining to guess the number.\n")
-            game_logic(rand_num, attempts)
-        print(f"You've run out of guesses. The number was {rand_num}.")
-        print("Please re-run the code to play again!")
+        print(f"Too high.")
+        return turns - 1
     elif guess < rand_num:
-        while attempts != 0:
-            attempts -= 1
-            print(f"Too low. Guess Again.")
-            print(f"You have {attempts} attempts remaining to guess the number.\n")
-            game_logic(rand_num, attempts)
-        print(f"You've run out of guesses. The number was {rand_num}.")
-        print("Please re-run the code to play again!")
+        print(f"Too low.")
+        return turns - 1
     else:
-        while attempts != 0:
-            attempts -= 1
-            print(f"Guess Again.")
-            print(f"You have {attempts} attempts remaining to guess the number.\n")
-            game_logic(rand_num, attempts)
-        print(f"You've run out of guesses. The number was {rand_num}.")
-        print("Please re-run the code to play again!")
-'''
-
-
+        print(f"Invalid answer.")
 
 def set_difficulty():
     """Sets either easy or hard difficulty."""
@@ -47,16 +28,26 @@ def set_difficulty():
     elif difficulty == "hard":
         return HARD_LEVEL_TURNS
 
+def game():
+    print("Welcome to the Number Guessing Game!")
+    print("Im thinking of a number between 1 and 100.\n")
 
+    rand_num = random.randint(1,100)
+    turns = set_difficulty()
 
-print("Welcome to the Number Guessing Game!")
-print("Im thinking of a number between 1 and 100.")
+    print(f"You have {turns} remaining to guess the number.\n")
 
-rand_num = random.randint(1,100)
-turns = set_difficulty()
+    guess = 0
+    while guess != rand_num:
+        print(f"You have {turns} attempts remaining to guess the number.")
+        guess = int(input("Make a guess: "))
 
-print(f"You have {turns} remaining to guess the number.\n")
+        turns = check_answer(guess, rand_num, turns)
+        if turns == 0:
+            print(f"You've run out of guesses. The number was {rand_num}.")
+            print("Please re-run the code to play again!\n")
+            return
+        elif guess != rand_num:
+            print("Guess Again.\n")
 
-guess = int(input("Make a guess: "))
-
-check_answer(guess, rand_num)
+game()
