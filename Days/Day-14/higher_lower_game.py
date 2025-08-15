3f8bc138df724e91ce8dea2ae8be5978a90329b8
JContreras14 <Jonathan.Contreras.JC1@gmail.com>
2025-08-14 23:59:10 -0400

    proj work

--- Days/Day-14/higher_lower_game.py
+++ Days/Day-14/higher_lower_game.py
@@ -1,3 +1,16 @@
 from art import logo
+from art import vs
+from game_data import data
+import random
+
+def random_generation():
+    """"""
+    random.randint(0, len(data) - 1)
+
 
 print(logo)
+
+print(vs)
+
+print(data[0]['name'])
+print(data[0]['follower_count'])
