af3ca4ab675c39facd8e0f4b2f0b930c6036c2ef
JContreras14 <Jonathan.Contreras.JC1@gmail.com>
2025-08-20 23:53:26 -0400

    redoing game logic

--- Days/Day-14/higher_lower_game.py
+++ Days/Day-14/higher_lower_game.py
@@ -2,17 +2,24 @@ from art import logo, vs
 from game_data import data
 import random
 
-def random_generation():
-    """Return a random piece of data from the list."""
-    return random.choice(data)
-
-def data_comparison():
-    return
+def format_data(data):
+    data_name = data["name"]
+    data_desc = data["description"]
+    data_country = data["country"]
+    return f"{data_name}, {data_desc}, from {data_country}"
 
 print(logo)
 
-choice_a = random_generation()
-choice_b = random_generation()
+data_a = random.choice(data)
+data_b = random.choice(data)
+if data_a == data_b:
+    data_b = random.choice(data)
+    
 
-print(choice_a['name'])
-print(choice_b)
+
+
+print(f"Compare A: {format_data(data_a)}.")
+print(f"Compare B: {format_data(data_b)}.")
+
+
+guess = input("Who has more followers? Type 'A' or 'B': ")
